e27bc4ce0fe1a34cc8015633fb6f9d8dac2539ea
arissutrisna <aris.soetrisna@gmail.com>
2021-11-20 17:22:40 +0700

    testing pertama

---
+++ index.js
@@ -0,0 +1,30 @@
+const nama = "Aris Sutrisna";
+// fungsi const -- constanta - tidak bisa dirubah - cuma bisa dirubah sekali
+
+let hobi = "Sepedaan";
+// fungsi let -- lepaskan bisa langsung dirubah berkali kali kalo ingin rubah isi
+
+hobi = "trading..";
+
+console.log("Saya suka",nama);
+// console.log(`${nama}, ${hobi}`);
+// Pakai curek sebelah angka 1
+
+// fungsi console.log -- cara manggil
+
+// CNTRL+shift+p -- fomratdocument -- prittier (Merapikan baris coding)
+// CTRL+` (sebelah angka 1) for open terminal
+// Ctrl+/ -- open comment
+// cara eksekusi -- node ./index.js
+// di gitbash open file location - klik kanan (git bash here) -- node index.js
+
+// setting ssh ke github web :
+// on desktop klik kanan - git bash here - ketik code ($ ssh-keygen -t rsa -b 4096 -C "aris.soetrisna@gmail.com") - set password (kosong)
+
+// Enter file in which to save the key (/c/Users/Aris Sutrisna/.ssh/id_rsa): --- lokasi derectory file
+
+// $ cat ~/.ssh/id_rsa.pub -- manggil ssh public
+// $ cat ~/.ssh/id_rsa -- manggil ssh private (gak boleh disebarin)
+
+// rm -rf ~/.ssh/*
+
